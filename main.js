4107c18e4709b194d7a2e26a9980871e8571d6b6
duongld2503 <ledinhduong2503@gmail.com>
2019-04-04 09:15:15 +0700

    thuchanh

---
+++ main.js
@@ -0,0 +1,14 @@
+var x = kiemTraSoNguyenTo(5);
+
+
+function kiemTraSoNguyenTo(n) {
+    let dem = 0;
+    for (i=1;i<=n;i++) {
+        if (n % i === 0) {
+            dem++;
+        }
+    }
+    if (dem === 2){
+        document.write(n)
+    }
+}
